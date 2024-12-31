e4f074114818efc54ea9e6c70fdcaa1624484354
Jacob Sandlund <jacob@jacobsandlund.com>
2024-12-31 09:44:33 -0500

    Fix line number style

--- src/features/line_numbers.rs
+++ src/features/line_numbers.rs
@@ -81,9 +81,9 @@ pub fn linenumbers_and_styles<'a>(
     let ((minus_number, plus_number), (minus_style, plus_style)) = match state {
         State::HunkMinus(_, _) => {
             line_numbers_data.line_number[Left] += increment as usize;
-            ((Some(nr_left), None), (minus_style, plus_style))
+            ((Some(nr_left), None), (minus_style, minus_style))
         }
-        State::HunkMinusWrapped => ((None, None), (minus_style, plus_style)),
+        State::HunkMinusWrapped => ((None, None), (minus_style, minus_style)),
         State::HunkZero(_, _) => {
             line_numbers_data.line_number[Left] += increment as usize;
             line_numbers_data.line_number[Right] += increment as usize;
@@ -92,9 +92,9 @@ pub fn linenumbers_and_styles<'a>(
         State::HunkZeroWrapped => ((None, None), (zero_style, zero_style)),
         State::HunkPlus(_, _) => {
             line_numbers_data.line_number[Right] += increment as usize;
-            ((None, Some(nr_right)), (minus_style, plus_style))
+            ((None, Some(nr_right)), (plus_style, plus_style))
         }
-        State::HunkPlusWrapped => ((None, None), (minus_style, plus_style)),
+        State::HunkPlusWrapped => ((None, None), (plus_style, plus_style)),
         _ => return None,
     };
     Some((
@@ -252,7 +252,7 @@ fn format_and_paint_line_number_field<'a>(
 
     let format_data = &line_numbers_data.format_data[side];
     let plus_file = &line_numbers_data.plus_file;
-    let style = &config.line_numbers_style_leftright[side];
+    let style = &styles[side];
 
     let mut ansi_strings = Vec::new();
     let mut suffix = "";
